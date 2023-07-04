65bbfa80b9387b42c08204cc0eb15ece6e23b81b
Ilya Osipov <brawo@inbox.ru>
2023-07-05 02:58:01 +0300

    Этап 1. Сервис статистики

---
+++ ewm-stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -0,0 +1,23 @@
+package ru.practicum.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hits")
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String app;
+    private String uri;
+    private String ip;
+    private LocalDateTime timestamp;
+}
